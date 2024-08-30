594a558a2b4b9ac3c3c682668693749f2fc1afd8
Matheus de Almeida <69125506+matheusmra@users.noreply.github.com>
2024-08-30 10:37:49 -0300

    Update ed03.c

--- Exercícios/ED´s/ED03/ed03.c
+++ Exercícios/ED´s/ED03/ed03.c
@@ -187,6 +187,40 @@ void ex0315() {
 
     for(int i = tam - 1; i >= 0; i--)
     {
+        if( ( 47 < palavra[i]) && (palavra[i] < 58))
+        {
+            contagem++;
+            printf("O caracter: [%c] eh um digito\n", palavra[i]);
+        }
+    }
+    printf("O total de caracteres que sao digitos eh: %d", contagem);
+
+    // encerrar
+    printf( "\nAperte ENTER para continuar!\n" );
+    getchar();
+}// Fim da função ex0215
+
+// Função para o exercício 0316
+// - ler uma cadeia de caracteres do teclado;
+// - contar e mostrar tudo o que não for dígito e também não for letra.
+
+void ex0316() {
+// identificacao
+    printf( "\nExercicio 0316:\n\n" );
+
+    // programa
+    char palavra [TAM_STR];
+    int tam = 0, contagem = 0;
+
+    printf( "Digite uma palavra: " );
+    scanf( "%s" , palavra);
+    getchar();
+    printf( "\n" );
+
+    tam = strlen(palavra);
+
+    for(int i = tam - 1; i >= 0; i--)
+    {
         if( ( 32 < palavra[i]) && (palavra[i] < 48) || (57 < palavra[i] && (palavra[i]) < 65) || (90 < palavra[i]) && (palavra[i] < 97) || (122 < palavra[i]) && (palavra[i] < 127))
         {
             contagem++;
@@ -198,196 +232,286 @@ void ex0315() {
     // encerrar
     printf( "\nAperte ENTER para continuar!\n" );
     getchar();
-}// Fim da função ex0215
-
-// Função para o exercício 0216
-// - ler dois valores inteiros do teclado e
-// - dizer se o primeiro é par e o segundo é ímpar.
-void ex0316(void) {
-    int numero1 = 0, numero2 = 0;
-    //Identificação
-    printf("\nExercicio 0216:\n\n");
-    //Inicio do Programa
-    printf("Digite o primeiro numero: ");
-    scanf("%d", &numero1);
-    printf("Digite o segundo numero: ");
-    scanf("%d", &numero2);
-    getchar(); // Limpar o buffer de entrada
-
-    if (numero1 % 2 == 0 && numero2 % 2 != 0) {
-        printf("O primeiro numero: %d eh par e o segundo numero: %d eh impar.\n", numero1, numero2);
-    } else if (numero1 % 2 != 0 && numero2 % 2 != 0) {
-        printf("Os dois numeros sao impares.\n");
-    } else if (numero1 % 2 != 0 && numero2 % 2 == 0) {
-        printf("O primeiro numero eh impar e o segundo eh par.\n");
-    } else if (numero1 % 2 == 0 && numero2 % 2 == 0) {
-        printf("Os dois numero sao pares.\n");
-    }
-    printf("Aperte ENTER para continuar!\n");
-    getchar();
 }// Fim da função ex0216
 
-// Função para o exercício 0217
-// - ler dois valores inteiros do teclado e
-// - dizer se o primeiro é ímpar e negativo, e se o segundo é par e positivo.
+// Função para o exercício 0317
+// - ler dois valores inteiros (a,b), limites para definirem um intervalo [a:b];
+// - ler uma quantidade (n) de valores inteiros a serem testados;
+// - repetir a leitura de outros tantos valores, quantos os indicados pela quantidade, um (x) por vez;
+// - contar e mostrar quantos dentre esses valores lidos (x) os que forem múltiplos de 5, e pertençam ao intervalo [a:b].
 void ex0317(void) {
-    int numero1 = 0, numero2 = 0;
-    printf("\nExercicio 0217:\n\n");
-    printf("Digite o primeiro numero: ");
-    scanf("%d", &numero1);
-    printf("Digite o segundo numero: ");
-    scanf("%d", &numero2);
-    getchar(); // Limpar o buffer de entrada
-
-    if (numero1 % 2 != 0 && numero1 < 0 && numero2 % 2 == 0 && numero2 > 0) {
-        printf("O primeiro numero: %d eh impar e negativo e o segundo numero: %d eh par e positivo.\n", numero1, numero2);
-    } else if (numero1 % 2 != 0 && numero1 < 0 && numero2 % 2 != 0 && numero2 < 0) {
-        printf("Ambos são impares e negativos.\n");
-    } else if (numero1 % 2 == 0 && numero1 < 0 && numero2 % 2 != 0 && numero2 > 0) {
-        printf("O primeiro numero eh par e negativo. O segundo eh impar e positivo.\n");
-    } else if (numero1 % 2 == 0 && numero1 > 0 && numero2 % 2 == 0 && numero2 > 0) {
-        printf("Ambos são pares e positivos.\n");
-    } else if (numero1 % 2 != 0 && numero1 < 0 && numero2 % 2 == 0 && numero2 < 0) {
-        printf("O primeiro numero é negativo e impar. O segundo eh par e negativo.\n");
-    }
-    printf("Aperte ENTER para continuar!\n");
+// identificacao
+    printf( "\nExercicio 0317:\n\n" );
+    int a = 0, b = 0, n = 0;
+    int count = 0;
+    printf( "Insira o primeiro valor do intervalo: " );
+    scanf( "%d" , &a );
     getchar();
-}// Fim da função ex0217
-// Função para o exercício 0218
-// - ler dois valores reais do teclado e
-// - dizer se o primeiro é menor, igual ou maior que a metade do segundo.
-void ex0318(void) {
-    double numero1 = 0.0, numero2 = 0.0;
-    printf("\nExercicio 0218:\n\n");
-    printf("Digite o primeiro numero: ");
-    scanf("%lf", &numero1);
-    printf("Digite o segundo numero: ");
-    scanf("%lf", &numero2);
-    getchar(); // Limpar o buffer de entrada
-
-    if (numero1 < (numero2 / 2)) {
-        printf("O numero %.2lf eh menor que a metade de %.2lf.\n", numero1, numero2);
-    } else if (numero1 == (numero2 / 2)) {
-        printf("O numero %.2lf eh a metade de %.2lf", numero1, numero2);
-    } else if (numero1 > (numero2 / 2)) {
-        printf("O numero %.2lf eh maior que a metade de %.2lf.\n", numero1, numero2);
-    }
-    printf("Aperte ENTER para continuar!\n");
+    printf( "Insira o valor final do intervalo: " );
+    scanf( "%d" , &b );
     getchar();
-}// Fim da função ex0218
-
-// Função para o exercício 0219
-// - ler três valores reais do teclado e
-// - dizer se o segundo está entre o primeiro e o último,
-// - quando esses dois forem diferentes entre si.
-// - OBS.: Notar a ordem dos testes.
-
-void ex0319(void) {
-    double numero1 = 0.0, numero2 = 0.0, numero3 = 0.0;
-    printf("\nExercicio 0219:\n\n");
-    printf("Digite o primeiro numero: ");
-    scanf("%lf", &numero1);
-    printf("Digite o segundo numero: ");
-    scanf("%lf", &numero2);
-    printf("Digite o terceiro numero: ");
-    scanf("%lf", &numero3);
-    getchar(); // Limpar o buffer de entrada
-
-    if (numero1 != numero3) {
-        if (numero2 > numero1 && numero2 < numero3) {
-            printf("O numero %.2lf esta entre os numeros: %.2lf e %.2lf.\n", numero2, numero1, numero3);
-        } else {
-            printf("O numero %.2lf nao esta entre os numeros.\n", numero2);
+    printf( "Insira a quantidade de valores: " );
+    scanf( "%d" , &n );
+    getchar();
+    printf( "\n" );
+    int x[n];
+    for(int i = 0; i < n; i=i+1 )
+    {
+        printf( "Digite o valor: " );
+        scanf( "%d" , &x[i] );
+        getchar();
+    }
+    printf( "\n" );
+    for(int i = 0; i < n; i++ )
+    {
+        if( (x[i] % 5 == 0) && (a <= x[i] && x[i] <= b) )
+        {
+            printf( "O numero %d eh multiplo de 5 e esta dentro do intervalo [ %d : %d ]\n" , x[i], a, b );
+            count++;
         }
-    } else {
-        printf("ERRO: O Primeiro numero deve ser diferente do Terceiro numero.\n");
     }
-    printf("Aperte ENTER para continuar!\n");
+
+    printf( "\n(%d) valores sao multiplos de 5 e pertecem ao intervalo\n", count );
+
+    // encerrar
+    printf( "\nAperte ENTER para continuar!\n" );
+    getchar();
+}// Fim da função ex0317
+// Função para o exercício 0318
+// - ler dois valores inteiros (a,b), limites para definirem um intervalo [a:b];
+// - ler uma quantidade (n) de valores inteiros a serem testados;
+// - ler outros tantos valores quantos os indicados pela quantidade, um (x) por vez;
+// - contar e mostrar quantos dentre esses valores lidos (x)os que forem múltiplos de 3, que não forem também múltiplos de 5,e pertençam ao intervalo [a:b].
+void ex0318() {
+// identificacao
+    printf( "\nExercicio 0318:\n\n" );
+    int a = 0, b = 0, n = 0;
+    int count = 0;
+    printf( "Insira o primeiro valor do intervalo: " );
+    scanf( "%d" , &a );
+    getchar();
+    printf( "Insira o valor final do intervalo: " );
+    scanf( "%d" , &b );
+    getchar();
+    printf( "Insira a quantidade de valores: " );
+    scanf( "%d" , &n );
+    getchar();
+    printf( "\n" );
+    int x[n];
+    for(int i = 0; i < n; i=i+1 )
+    {
+        printf( "Digite o valor: " );
+        scanf( "%d" , &x[i] );
+        getchar();
+    }
+    printf( "\n" );
+    for(int i = 0; i < n; i++ )
+    {
+        if( (x[i] % 3 == 0) && (x[i] % 5 != 0) && (a <= x[i] && x[i] <= b) )
+        {
+            printf( "O numero %d eh multiplo de 3, nao eh multiplo de 5 e esta dentro do intervalo [ %d : %d ]\n" , x[i], a, b );
+            count++;
+        }
+    }
+    printf( "\n(%d) valores sao multiplos de 3, nao sao multiplos de 5 e pertecem ao intervalo\n", count );
+    // encerrar
+    printf( "\nAperte ENTER para continuar!\n" );
+    getchar();
+}// Fim da função ex0318
+
+// Função para o exercício 0319
+// - ler dois valores reais (a e b), o primeiro (a) menor que o segundo (b), confirmadamente, para definirem um intervalo aberto (a:b);
+// - ler a quantidade (n) de valores reais a serem testados, e ler outros tantos valores (x) quantos os indicados por essa quantidade;
+// - contar e mostrar todos os valores lidos, pertencentes ao do intervalo,cujas partes inteiras forem ímpares.
+// DICA: Usar conformação de tipo (type casting) para isolar a parte inteira (int),antes de testar se é ímpar (ver 0110).
+
+
+void ex0319() {
+    // identificacao
+    printf( "\nExercicio 0319:\n\n" );
+    double a = 0.0, b = 0.0;
+    int n = 0, count = 0;
+    printf( "Insira o primeiro valor do intervalo: " );
+    scanf( "%lf" , &a );
+    getchar();
+    printf( "Insira o valor final do intervalo: " );
+    scanf( "%lf" , &b );
+    getchar();
+    if( a >= b )
+    {
+        printf( "\nERRO: A valores invalidos! Certifique-se de que a < b! \n" );
+    }
+    else
+    {
+        printf( "Insira a quantidade de valores: " );
+        scanf( "%d" , &n );
+        getchar();
+        printf( "\n" );
+        double x[n];
+        for(int i = 0; i < n; i=i+1 )
+        {
+            printf( "Digite o valor: " );
+            scanf( "%lf" , &x[i] );
+            getchar();
+        }
+        printf( "\n" );
+        for (int i = 0; i < n; i++)
+        {
+            if( a < x[i] && x[i] < b )
+            {
+                int inteiro = (int)x[i];
+                if( inteiro % 2 == 1 )
+                {
+                    printf( "%.2lf eh impar e pertece ao intervalo\n" , x[i] );
+                    count++;
+                }
+
+            }
+        }
+
+        printf( "\n%d valores com partes inteiras impares pertencem ao intervalo\n" , count );
+    }
+
+    // encerrar
+    printf( "\nAperte ENTER para continuar!\n" );
     getchar();
 }// Fim da função ex0219
 
 // Função para o exercício 0220
-// - ler três valores reais do teclado e
-// - dizer se o segundo não está entre o primeiro e o último,
-// - quando todos forem diferentes entre si.
-void ex0320(void) {
-    double numero1 = 1.0, numero2 = 1.0, numero3 = 1.0;
-    printf("\nExercicio 0220:\n\n");
-    printf("Digite o primeiro numero: ");
-    scanf("%lf", &numero1);
-    printf("Digite o segundo numero: ");
-    scanf("%lf", &numero2);
-    printf("Digite o terceiro numero: ");
-    scanf("%lf", &numero3);
-    getchar(); // Limpar o buffer de entrada
+// - ler dois valores reais (a e b), maiores que 0 e menores que 1, confirmadamente, para definirem um intervalo aberto (a:b);
+// - ler uma quantidade (n) de valores reais a serem testados, e ler outros tantos valores quantos os indicados por essa quantidade;
+// - contar e mostrar todos os valores lidos que tenham suas partes fracionárias fora do intervalo ]a:b[.
+// DICA: Usar conformação de tipo (type casting) para isolar a parte inteira (int),e obter a parte fracionária mediante a subtração da parte inteira, antes de testar.
 
-    if (numero1 != numero2 && numero1 != numero3 && numero2 != numero3) {
-        if (numero2 < numero1 || numero2 > numero3) {
-            printf("O numero %.2lf nao esta entre os numeros: %.2lf e %.2lf.\n", numero2, numero1, numero3);
-        } else {
-            printf("O numero %.2lf esta entre os numeros: %.2lf e %.2lf.\n", numero2, numero1, numero3);
-        }
-    } else {
-        printf("ERRO: Os tres numeros devem ser diferentes!\n");
+void ex0320() {
+// identificacao
+    printf( "\nExercicio 0320:\n\n" );
+    double a = 0.0, b = 0.0;
+    int n = 0, count = 0;
+
+    printf( "Insira o primeiro valor do intervalo: " );
+    scanf( "%lf" , &a );
+    getchar();
+
+    printf( "Insira o valor final do intervalo: " );
+    scanf( "%lf" , &b );
+    getchar();
+
+
+    if( a <= 0 || a >= 1 || b <= 0 || b >= 1 || a >= b ) //
+    {
+        printf( "\n%s\n" , "ERRO: O valor e inicial e final nao sao maiores que 0 e menores que 1" );
     }
-    printf("Aperte ENTER para continuar!\n");
+    else
+    {
+        printf( "Insira a quantidade de valores: " );
+        scanf( "%d" , &n );
+        getchar();
+        printf( "\n" );
+
+        double x[n];
+
+        for(int i = 0; i < n; i=i+1 )
+        {
+            printf( "Digite o valor: " );
+            scanf( "%lf" , &x[i] );
+            getchar();
+        }
+        printf( "\n" );
+
+        for(int i = 0; i < n; i=i+1 )
+        {
+            x[i] = x[i] - (int)x[i];
+        }
+
+        for (int i = 0; i < n; i++)
+        {
+            if( x[i] < a || x[i] > b ) // nao pertence ao intervalo
+            {
+                printf( "(%.2lf) nao pertece ao intervalo\n" , x[i] );
+                count++;
+            }
+        }
+
+        printf( "\n%d valores tem partes fracionarias que nao pertencem ao intervalo\n" , count );
+    }
+
+    // encerrar
+    printf( "\nAperte ENTER para continuar!\n" );
     getchar();
 }// Fim da função ex0220
 
 // Função para o exercício 02E1
-// - ler três valores literais (caracteres) do teclado e
-// - dizer se o primeiro valor lido está entre os outros dois, ou se é igual a um deles.
+// - ler uma linha do teclado;
+// - separar em outra cadeia de caracteres e mostrar todos os símbolos não alfanuméricos (letras ou dígitos) na cadeia de caracteres.
 
-void ex03E1(void) {
-    char a = 'a', b = 'b', c = 'c';
-    printf("\nExercicio 02E1:\n\n");
-    printf("Digite o primeiro caractere: ");
-    scanf(" %c", &a);
-    printf("Digite o segundo caractere: ");
-    scanf(" %c", &b);
-    printf("Digite o terceiro caractere: ");
-    scanf(" %c", &c);
-    getchar(); // Limpar o buffer de entrada
+void ex03E1()
+{
+    // identificacao
+    printf( "\nExercicio 03E1:\n\n" );
 
-    if (a == b || a == c) {
-        printf("%c eh igual a pelo menos um dos outros valores.\n", a);
-    } else if ((a > b && a < c) || (a < b && a > c)) {
-        printf("%c esta entre %c e %c.\n", a, b, c);
-    } else {
-        printf("%c nao esta entre %c e %c.\n", a, b, c);
-    }
-    printf("Aperte ENTER para continuar!\n");
-    getchar();
-}// Fim da função ex02E1
+    // programa
+    char palavra[TAM_STR];
 
-// Função para o exercício 02E2
-// - ler três valores literais (caracteres) do teclado e
-// - dizer se o primeiro valor lido está fora do intervalo definido pelos outros dois,
-// - se esses forem diferentes entre si.
-// - OBS.: Notar que não haverá garantias de ser o segundo menor que o terceiro.
-void ex03E2(void) {
-    char a = 'a', b = 'b', c = 'c';
-    printf("\nExercicio 02E2:\n\n");
-    printf("Digite o primeiro caractere: ");
-    scanf(" %c", &a);
-    printf("Digite o segundo caractere: ");
-    scanf(" %c", &b);
-    printf("Digite o terceiro caractere: ");
-    scanf(" %c", &c);
-    getchar(); // Limpar o buffer de entrada
+    printf( "Digite uma linha:\n" );
+    fgets(palavra, sizeof(palavra), stdin);
 
-    if (b != c) {
-        if (!(((b < a) && (a < c)) || ((b > a) && (a > c)))) {
-            printf("O caractere (%c) esta fora do intervalo (%c : %c).\n", a, b, c);
-        } else {
-            printf("O caractere (%c) esta dentro do intervalo (%c : %c).\n", a, b, c);
+    for(int i = 0; palavra[i] != '\0'; i++ )
+    {
+        if( !(isalnum(palavra[i])) && !(isspace(palavra[i])) )
+        {
+            printf( "%c%s\n" , palavra[i], " nao eh alfanumerico." );
         }
-    } else {
-        printf("Tem dois caracteres iguais.\n");
     }
-    printf("Aperte ENTER para continuar!\n");
+
+    // encerrar
+    printf( "\nAperte ENTER para continuar!\n" );
     getchar();
-}// Fim da função ex02E2
+    getchar();
+} // fim exercicio03E1
+// - Exercicio extra 03E2
+//- ler uma cadeia de caracteres do teclado;
+//- dizer se a sequência contém apenas símbolos que NÃO são letras
+void ex03E2()
+{
+    // identificacao
+    printf( "\nExercicio 03E2:\n\n" );
+
+    // programa
+    char palavra[TAM_STR];
+    int i = 0, tam = 0, var = 0;
+
+    printf( "Digite uma sequencia de caracteres:\n" );
+    scanf( "%s" , palavra );
+
+    tam = strlen(palavra);
+
+    for( i = 0; i < tam; i++ )
+    {
+        if( isalpha(palavra[i]) )
+        {
+            var = 0;
+        }
+        else
+        {
+            var = 1;
+        }
+    }
+
+    if(var)
+    {
+        printf("\nA cadeia de caracteres \"%s\" so tem simbolos\n" , palavra);
+    }
+    else
+    {
+        printf("\nA cadeia de caracteres \"%s\" tem letra(s)\n" , palavra);
+    }
+
+    // encerrar
+    printf( "\nAperte ENTER para continuar!\n" );
+    getchar();
+    getchar();
+} // fim função 03E2
 
 // Função principal
 int main(void) {
