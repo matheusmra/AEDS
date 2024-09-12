9b3540014230e73640f48b26c0fb6bb6a3dff924
Matheus de Almeida <69125506+matheusmra@users.noreply.github.com>
2024-09-12 20:35:00 -0300

    Update ed05.c

--- Exercícios/ED´s/ED05/ed05.c
+++ Exercícios/ED´s/ED05/ed05.c
@@ -88,10 +88,10 @@ void ex0513() {
 }// Fim da fun��o ex0413
 
 // Fun��o para o exerc�cio 0414
-Incluir função e método (0514) para:
-para ler uma quantidade inteira do teclado e, mediante um procedimento,
-testar a função para gerar valores crescentes nos denominadores
-(sequência dos inversos) múltiplos de 4.
+// Incluir função e método (0514) para:
+// para ler uma quantidade inteira do teclado e, mediante um procedimento,
+// testar a função para gerar valores crescentes nos denominadores
+// (sequência dos inversos) múltiplos de 4.
 void ex0514() {
 // identificacao
     printf( "\nExercicio 0514:\n\n" );
@@ -111,12 +111,12 @@ void ex0514() {
 }// Fim da fun��o ex0414
 
 // Fun��o para o exerc�cio 0415
-Incluir função e método (0515) para:
-para ler um valor real real (x) do teclado;
-para ler uma quantidade inteira do teclado e, mediante um procedimento,
-testar a função para gerar valores ímpares crescentes nos denominadores
-da sequência: 1 1/x1 1/x3 1/x5 ...
-DICA: Usar da biblioteca <math.h> a função pow ( x, y ) para calcular a potência
+// Incluir função e método (0515) para:
+// para ler um valor real real (x) do teclado;
+// para ler uma quantidade inteira do teclado e, mediante um procedimento,
+// testar a função para gerar valores ímpares crescentes nos denominadores
+// da sequência: 1 1/x1 1/x3 1/x5 ...
+// DICA: Usar da biblioteca <math.h> a função pow ( x, y ) para calcular a potência
 void ex0515() {
 // identificacao
     printf( "\nExercicio 0515:\n\n" );
@@ -137,11 +137,11 @@ void ex0515() {
 }// Fim da fun��o ex0415
 
 // Fun��o para o exerc�cio 0416
-Incluir função e método (0516) para
-calcular a soma dos primeiros valores positivos começando no valor 4,
-múltiplos de 4 e não múltiplos de 5.
-Testar essa função para quantidades diferentes e mostrar os resultados em outro método.
-Exemplo: n = 5 => 4 + 8 + 12 + 16 + 24
+// Incluir função e método (0516) para
+// calcular a soma dos primeiros valores positivos começando no valor 4,
+// múltiplos de 4 e não múltiplos de 5.
+// Testar essa função para quantidades diferentes e mostrar os resultados em outro método.
+// Exemplo: n = 5 => 4 + 8 + 12 + 16 + 24
 
 
 void ex0516() {
@@ -164,12 +164,12 @@ void ex0516() {
 }// Fim da fun��o ex0416
 
 // Fun��o para o exerc�cio 0417
-Incluir função e método (0517) para
-calcular a soma dos inversos (1/x) dos primeiros valores positivos,
-começando no valor 3, múltiplos de 3 e não múltiplos de 5.
-Testar essa função para quantidades diferentes e
-mostrar os resultados em outro método.
-Exemplo: n = 5 => 1/3 + 1/6 + 1/9 + 1/12 + 1/18
+// Incluir função e método (0517) para
+// calcular a soma dos inversos (1/x) dos primeiros valores positivos,
+// começando no valor 3, múltiplos de 3 e não múltiplos de 5.
+// Testar essa função para quantidades diferentes e
+// mostrar os resultados em outro método.
+// Exemplo: n = 5 => 1/3 + 1/6 + 1/9 + 1/12 + 1/18
 void ex0517(void) {
 // identificacao
     printf( "\nExercicio 0517:\n\n" );
@@ -187,11 +187,11 @@ void ex0517(void) {
 
 }// Fim da fun��o ex0417
 // Fun��o para o exerc�cio 0518
-Incluir função e método (0518) para
-calcular a soma da adição dos primeiros números naturais começando no valor 4.
-Testar essa função para quantidades diferentes de valores e
-mostrar os resultados em outro método.
-Exemplo: n = 5 => 4 + 5 + 7 + 10 + 14
+// Incluir função e método (0518) para
+// calcular a soma da adição dos primeiros números naturais começando no valor 4.
+// Testar essa função para quantidades diferentes de valores e
+// mostrar os resultados em outro método.
+// Exemplo: n = 5 => 4 + 5 + 7 + 10 + 14
 void ex0518() {
 // identificacao
     printf( "\nExercicio 0518:\n\n" );
@@ -208,11 +208,11 @@ void ex0518() {
 }// Fim da fun��o ex0418
 
 // Fun��o para o exerc�cio 0519
-Incluir função e método (0519) para
-calcular a soma dos quadrados da adição dos números naturais começando no valor 4.
-Testar essa função para quantidades diferentes de valores e
-e mostrar os resultados em outro método.
-Exemplo: n = 5 => 16 + 25 + 36 + 49 + 64
+// Incluir função e método (0519) para
+// calcular a soma dos quadrados da adição dos números naturais começando no valor 4.
+// Testar essa função para quantidades diferentes de valores e
+// e mostrar os resultados em outro método.
+// Exemplo: n = 5 => 16 + 25 + 36 + 49 + 64
 void ex0519() {
     // identificacao
     printf( "\nExercicio 0519:\n\n" );
@@ -230,11 +230,11 @@ void ex0519() {
 }// Fim da fun��o ex0419
 
 // Fun��o para o exerc�cio 0420
-Incluir função e método (Exemplo0520) para
-calcular a soma dos inversos (1/x) das adições de números naturais terminando no valor 4.
-Testar essa função para quantidades diferentes de valores
-e mostrar os resultados em outro método.
-Exemplo: n = 5 => 1/14 + 1/10 + 1/7 + 1/5 + 1/4
+// Incluir função e método (Exemplo0520) para
+// calcular a soma dos inversos (1/x) das adições de números naturais terminando no valor 4.
+// Testar essa função para quantidades diferentes de valores
+// e mostrar os resultados em outro método.
+// Exemplo: n = 5 => 1/14 + 1/10 + 1/7 + 1/5 + 1/4
 
 void ex0520() {
 // identificacao
@@ -244,10 +244,10 @@ void ex0520() {
 }// Fim da fun��o ex0420
 
 // Fun��o para o exerc�cio 04E1
-Incluir função e método (Exemplo05E1) para
-ler um número inteiro do teclado (n) e,
-mediante o uso da função, calcular e mostrar o fatorial desse valor em outro método:
-n! = n * (n-1) * (n-2) * ... * 3 * 2 * 1 se n>0
+// Incluir função e método (Exemplo05E1) para
+// ler um número inteiro do teclado (n) e,
+// mediante o uso da função, calcular e mostrar o fatorial desse valor em outro método:
+// n! = n * (n-1) * (n-2) * ... * 3 * 2 * 1 se n>0
 
 void ex05E1()
 {
@@ -256,10 +256,10 @@ void ex05E1()
 
 } // fim exercicio04E1
 // Fun��o para o exerc�cio 04E2
-Incluir função e método (Exemplo05E2) para
-ler uma quantidade inteira do teclado (n) e,
-mediante o uso da função, calcular e mostrar o resultado em outro método de
-f ( n ) = (1+3/2!) * (1+5/4!) * (1+7/8!) * 
+// Incluir função e método (Exemplo05E2) para
+// ler uma quantidade inteira do teclado (n) e,
+// mediante o uso da função, calcular e mostrar o resultado em outro método de
+// f ( n ) = (1+3/2!) * (1+5/4!) * (1+7/8!) * 
 void ex05E2()
 {
     // identificacao
