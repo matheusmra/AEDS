852ec03be2bb5ef96db737d0f2a0938ff643d7a7
Matheus de Almeida <69125506+matheusmra@users.noreply.github.com>
2024-09-15 14:41:13 -0300

    Update ed05.c

--- Exercícios/ED´s/ED05/ed05.c
+++ Exercícios/ED´s/ED05/ed05.c
@@ -161,6 +161,7 @@ void ex0514() {
 // testar a função para gerar valores ímpares crescentes nos denominadores
 // da sequência: 1 1/x1 1/x3 1/x5 ...
 // DICA: Usar da biblioteca <math.h> a função pow ( x, y ) para calcular a potência
+
 void ex0515() {
 // identificacao
     printf( "\nExercicio 0515:\n\n" );
@@ -179,29 +180,29 @@ void ex0515() {
 
 int fun0516(int v1) {
     int soma = 0;
+    int x    = 4;
 
     while (v1 >= 1) {
-        if ((v1 % 4 == 0) && (v1 % 5 != 0)) {
-            soma = soma+4;
+        if ((x % 4 == 0) && (x % 5 != 0)) {
+            soma = soma+x;
             v1--;
         }
+        x = x + 1;
     }
     return(soma);
 }
 
 void ex0516() {
     printf("\nExercicio 0516:\n\n");
-    int n = 0, total = 0;
+    int n = 0;
     printf("Digite o numero de termos: ");
     scanf("%d", &n);
-    printf("Teste");
-    total = fun0516(n);
-    printf("%d", total);
+    printf("\nA soma dos multiplos de 4 que nao sao multiplos de 5: %d\n", fun0516(n));
     getchar();
     printf("\nAperte ENTER para continuar!\n");
-    getchar();
-
-}// Fim da fun��o ex0416
+    getchar(); // Para esperar o usuário pressionar ENTER
+}
+// Fim da fun��o ex0416
 
 // Fun��o para o exerc�cio 0417
 // Incluir função e método (0517) para
@@ -210,24 +211,69 @@ void ex0516() {
 // Testar essa função para quantidades diferentes e
 // mostrar os resultados em outro método.
 // Exemplo: n = 5 => 1/3 + 1/6 + 1/9 + 1/12 + 1/18
-void ex0517(void) {
-// identificacao
-    printf( "\nExercicio 0517:\n\n" );
+double met0517(int v1)
+{
+    double soma = 0;
+    int    x    = 3;
 
-    printf( "\nAperte ENTER para continuar!\n" );
+    while (1 <= v1)
+    {
+        if( (x % 3 == 0) && (x % 5 != 0) )
+        {
+            soma = soma + (1.0/x);
+            v1 = v1- 1;
+        }
+        x = x + 1;
+    }
+    return ( soma );
+}
+
+void ex0517()
+{
+    // identificacao
+    printf("\nExercicio 0517:\n\n");
+
+    // programa
+    int n = 0;
+    printf( "\nDigite uma quantidade: " );
+    scanf("%d", &n);
+
+    printf( "\n%s%lf\n", "Soma = ", met0517(n) );
+
+    // encerrar
+    printf("\n%s\n", "Aperte ENTER para continuar!");
     getchar();
-
-}// Fim da fun��o ex0417
+} // fim exercicio0517// Fim da fun��o ex0417
 // Fun��o para o exerc�cio 0518
 // Incluir função e método (0518) para
 // calcular a soma da adição dos primeiros números naturais começando no valor 4.
 // Testar essa função para quantidades diferentes de valores e
 // mostrar os resultados em outro método.
 // Exemplo: n = 5 => 4 + 5 + 7 + 10 + 14
+int met0518(int v1)
+{
+    int soma = 0;
+    int num  = 4;
+    int x    = 1;
+
+    while( v1 > 0 )
+    {
+
+        soma = soma + num;
+        num = num + x;
+        x = x + 1;
+        v1 = v1 - 1;
+    }
+    return ( soma );
+}
 void ex0518() {
 // identificacao
     printf( "\nExercicio 0518:\n\n" );
-
+    int n = 0;
+    printf("\nDigite um numero:");
+    scanf("%d", &n);
+    printf("\nA soma eh igual a: %d", met0518(n));
+    getchar();
     printf( "\nAperte ENTER para continuar!\n" );
     getchar();
 }// Fim da fun��o ex0418
@@ -238,10 +284,30 @@ void ex0518() {
 // Testar essa função para quantidades diferentes de valores e
 // e mostrar os resultados em outro método.
 // Exemplo: n = 5 => 16 + 25 + 36 + 49 + 64
+int met0519(int v1)
+{
+    int soma = 0;
+    int num  = 4;
+    int x    = 1;
+
+    while( v1 > 0 )
+    {
+
+        soma = soma + pow(num, 2);
+        num = num + x;
+        x = x + 1;
+        v1 = v1 - 1;
+    }
+    return ( soma );
+}
 void ex0519() {
     // identificacao
     printf( "\nExercicio 0519:\n\n" );
-
+    int n = 0;
+    printf("\nDigite um numero:");
+    scanf("%d", &n);
+    printf("\nA soma eh igual a: %d", met0519(n));
+    getchar();
     printf( "\nAperte ENTER para continuar!\n" );
     getchar();
 
@@ -253,17 +319,18 @@ void ex0519() {
 // Testar essa função para quantidades diferentes de valores
 // e mostrar os resultados em outro método.
 // Exemplo: n = 5 => 1/14 + 1/10 + 1/7 + 1/5 + 1/4
-void fun0520(int v1) {
-int controle = 4, n = 1;
-controle = controle * v1;
-    for (int i = 1; i <= v1; i++) {
-        controle = controle - 4;
-        printf("\nO numero: (%d/%d) eh multiplo de quatro.",n,controle);
+double fun0520(int v1) {
+    double soma = 0.0;
+    double num  = 4.0;
+    double x    = 1;
+    for(int i = 1; i <= v1; i = i + 1)
+    {
+        soma = soma + (1.0/num);
+        num = num + x;
+        x = x + 1;
     }
-    printf("\n");
 
-
-
+    return ( soma );
 }
 
 void ex0520() {
@@ -272,7 +339,7 @@ void ex0520() {
     int n = 0;
     printf("Digite o numero de termos:");
     scanf("%d",&n);
-    fun0520(n);
+    printf("\nSoma igual a: %d", fun0520(n));
     getchar();
     // encerrar
     printf( "\nAperte ENTER para continuar!\n" );
