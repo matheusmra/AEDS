2a7fb6ea2365975bd5d52bb52eea7b313d85ba8a
Matheus de Almeida <69125506+matheusmra@users.noreply.github.com>
2024-10-30 18:04:25 -0300

    Update ed12.cpp

--- Estudo dirigido/ED12/ed12.cpp
+++ Estudo dirigido/ED12/ed12.cpp
@@ -18,6 +18,12 @@ using namespace std;
 // Tamanho max string
 const int TAM_STR = 80;
 // Função para mostrar o menu de opções
+
+void close(){
+    cout << "\nAperte ENTER para continuar!\n";
+    getchar();
+    getchar();
+}
 void menuOpcoes() {
    cout << "\nEscolha alguma das opcoes a seguir:\n\n";
    cout << "  0 - Encerrar programa\n";
@@ -43,7 +49,7 @@ public:
         this->rows = m;
         this->cols = n;
         data = new int*[this->rows];
-        for (int i = 0; i < this->rows; ++i) {
+        for (int i = 0; i < this->rows; ++i){
             data[i] = new int[this->cols];
         }
     }
@@ -52,19 +58,21 @@ public:
     }
 
 void randomIntGenerate(int inferior, int superior) {
-    for (int i = 0; i < rows; i++) {
-        for (int j = 0; j < cols; j++) {
+    for(int i = 0; i < rows; i++){
+        for(int j = 0; j < cols; j++){
             data[i][j] = rand() % (superior - inferior + 1) + inferior;
         }
     }
 }
 
+
+
 void writeToFile(const char* filename) {
-        std::ofstream file(filename);
+        ofstream file(filename);
         if (file.is_open()) {
             file << rows << " " << cols << "\n";
-            for (int i = 0; i < rows; ++i) {
-                for (int j = 0; j < cols; ++j) {
+            for (int i = 0; i < rows; ++i){
+                for (int j = 0; j < cols; ++j){
                     file << data[i][j] << " ";
                 }
                 file << "\n";
@@ -75,7 +83,128 @@ void writeToFile(const char* filename) {
             cout << "Erro ao abrir o arquivo.\n";
         }
     }
+void readFromFile(const char* filename){
+        ifstream file(filename);
+        if (file.is_open()) {
+            file >> rows >> cols;
+            data = new int*[rows];
+            for (int i = 0; i < rows; ++i){
+                data[i] = new int[cols];
+                for (int j = 0; j < cols; ++j){
+                    file >> data[i][j];
+                }
+            }
+            file.close();
+            cout << "Matriz carregada do arquivo \"" << filename << "\".\n";
+        } else {
+            cout << "Erro ao abrir o arquivo.\n";
+        }
+    }
 
+void scalar(int constante) {
+        for(int i = 0; i < rows; i++){
+            for(int j = 0; j < cols; j++){
+                data[i][j] *= constante;
+            }
+        }
+    }
+
+void printar() {
+        for (int i = 0; i < rows; i++){
+            for (int j = 0; j < cols; j++){
+                cout << data[i][j] << " ";
+            }
+            cout << endl;
+        }
+    }
+
+
+bool identidade(){
+        if(rows != cols){
+            return false;
+        }
+        for(int i = 0; i < rows; i++){
+            for(int j = 0; j < cols; j++){
+                if(i == j){
+                    if(data[i][j] != 1){
+                        return false;
+                    }
+                }else{
+                    if(data[i][j] != 0){
+                        return false;
+                    }
+                }
+            }
+        }
+        return true;
+    }
+
+bool operator==(const Matrix& outro) const {
+        if(rows != outro.rows || cols != outro.cols) {
+            return false;
+        }
+        for(int i = 0; i < rows; i++){
+            for(int j = 0; j < cols; j++){
+                if(data[i][j] != outro.data[i][j]){
+                    return false;
+                }
+            }
+        }
+        return true;
+    }
+
+Matrix add(const Matrix& other){
+        Matrix result(rows, cols);
+        for(int i = 0; i < rows; i++){
+            for(int j = 0; j < cols; j++){
+                result.data[i][j] = data[i][j] + other.data[i][j];
+            }
+        }
+        return result;
+    }
+
+void addRows(int row1, int row2, int constant) {
+        for(int j = 0; j < cols; j++){
+            data[row1][j] += data[row2][j] * constant;
+        }
+    }
+
+void subtractRow(int row1, int row2, int constant){
+        for(int j = 0; j < cols; j++){
+            data[row1][j] -= data[row2][j] * constant;
+        }
+    }
+int searchRow(int procurado) {
+    for(int i = 0; i < rows; i++){
+    for(int j = 0; j < cols; j++){
+            if(data[i][j] == procurado){
+                return i;
+        }
+    }
+    }
+    return -1;
+}
+
+int searchCol(int procurado){
+    for(int i =0; i < rows; i++){
+    for(int j = 0; j < cols; j++){
+        if(data[i][j] == procurado){
+            return j;
+        }
+    }
+    }
+    return -1;
+}
+
+Matrix transpose() {
+        Matrix transpose(cols, rows);
+        for(int i = 0; i < rows; i++){
+        for(int j = 0; j < cols; j++){
+                transpose.data[j][i] = data[i][j];
+            }
+        }
+        return transpose;
+    }
 
 };
 
@@ -89,7 +218,6 @@ void writeToFile(const char* filename) {
 // DICA: Usar a função rand( ), mas tentar limitar valores muito grandes.
 // Exemplo: valor = arranjo.randomIntGenerate ( inferior, superior );
 
-
 int ex1211() {
 // identificacao
     cout << "\nExercicio 1211:\n\n";
@@ -101,9 +229,7 @@ int ex1211() {
     Matrix matrix(l,c);
     matrix.randomIntGenerate(1,100);
     matrix.writeToFile("DADOS.TXT");
-    getchar();
-    cout << "\nAperte ENTER para continuar!\n";
-    getchar();
+    close();
 }
 // fim exercicio1011
 
@@ -121,11 +247,23 @@ int ex1211() {
 int ex1212() {
     // Identificação
     cout << "\nExercicio 1212:\n\n";
+    Matrix matrix(0, 0);
+    matrix.readFromFile("DADOS.TXT");
 
-    cout << "\nAperte ENTER para continuar!\n";
-    getchar(); // Pausa para esperar ENTER
+    cout << "\nMatriz original:\n";
+    matrix.printar();
 
-    return 0; // Encerra com sucesso
+    int constante = 0;
+    cout << "\nDigite a constante para escalar a matriz: ";
+    cin >> constante;
+
+    matrix.scalar(constante);
+
+    cout << "\nMatriz escalada:\n";
+    matrix.printar();
+    close();
+
+
 }// Fim da função ex1012
 
 // Função para o exercício 1013
@@ -141,8 +279,17 @@ int ex1212() {
 void ex1213() {
 // identificacao
     cout << "\nExercicio 1213:\n\n" ;
-    cout << "\nAperte ENTER para continuar!\n";
-    getchar(); // Pausa para esperar ENTER
+    Matrix matrix(0, 0);
+    matrix.readFromFile("DADOS.TXT");
+    cout << "\nMatriz:\n";
+    matrix.printar();
+    int teste = matrix.identidade();
+    if(teste){
+        cout << "A matriz eh uma matriz identidade";
+    }else{
+        cout << "A matriz nao eh uma matriz identidade";
+    }
+    close();
 
 }// Fim da função ex1013
 
@@ -156,10 +303,20 @@ void ex1213() {
 void ex1214() {
 // identificacao
     cout << "\nExercicio 1214:\n\n" ;
-
-    cout << "\nAperte ENTER para continuar!\n";
-    getchar();
-    getchar();
+    Matrix matrix1(0, 0);
+    matrix1.readFromFile("DADOS.TXT");
+    cout << "\nMatriz 01:\n";
+    matrix1.printar();
+    Matrix matrix2(0, 0);
+    matrix2.readFromFile("DADOS.TXT");
+    cout << "\nMatriz 02:\n";
+    matrix2.printar();
+    if(matrix1 == matrix2){
+        cout << "As duas matrizes sao iguais!";
+    }else{
+        cout << "As matrizes sao diferentes!";
+    }
+    close();
 }// Fim da função ex1014
 
 // Função para o exercício 1015
@@ -174,10 +331,18 @@ void ex1214() {
 void ex1215() {
 // identificacao
     cout << "\nExercicio 1215:\n\n" ;
-
-    cout << "\nAperte ENTER para continuar!\n";
-    getchar();
-    getchar();
+    Matrix matrix1(0, 0);
+    matrix1.readFromFile("DADOS.TXT");
+    cout << "\nMatriz 01:\n";
+    matrix1.printar();
+    Matrix matrix2(0, 0);
+    matrix2.readFromFile("DADOS.TXT");
+    cout << "\nMatriz 02:\n";
+    matrix2.printar();
+    Matrix soma = matrix1.add(matrix2);
+    cout << "\nSoma das matrizes:\n";
+    soma.printar();
+    close();
 }// Fim da função ex0915
 
 // Função para o exercício 0916
@@ -191,9 +356,20 @@ void ex1215() {
 void ex1216() {
 // identificacao
     cout << "\nExercicio 1216:\n\n" ;
-
-    cout << "\nAperte ENTER para continuar!\n";
-    getchar(); // Pausa para esperar ENTER
+    Matrix matrix(0, 0);
+    matrix.readFromFile("DADOS.TXT");
+    cout << "\nMatriz:\n";
+    matrix.printar();
+    int row1 = 0,row2 = 0,constant = 0;
+    cout << "Linha 01:";
+    cin >> row1;
+    cout << "\nLinha 02:";
+    cin >> row2;
+    cout << "Constante:";
+    cin >> constant;
+    matrix.addRows(row1,row2,constant);
+    matrix.printar();
+    close();
 }// Fim da função ex1016
 
 // Função para o exercício 1017
@@ -208,9 +384,20 @@ void ex1216() {
 void ex1217() {
 // identificacao
     cout << "\nExercicio 1217:\n\n" ;
-
-    cout << "\nAperte ENTER para continuar!\n";
-    getchar(); // Pausa para esperar ENTER
+    Matrix matrix(0, 0);
+    matrix.readFromFile("DADOS.TXT");
+    cout << "\nMatriz:\n";
+    matrix.printar();
+    int row1 = 0,row2 = 0,constant = 0;
+    cout << "Linha 01:";
+    cin >> row1;
+    cout << "\nLinha 02:";
+    cin >> row2;
+    cout << "Constante:";
+    cin >> constant;
+    matrix.subtractRow(row1,row2,constant);
+    matrix.printar();
+    close();
 }// Fim da função ex0917
 
 // Função para o exercício 1018
@@ -228,10 +415,20 @@ void ex1217() {
 void ex1218() {
 // identificacao
     cout << "\nExercicio 1218:\n\n" ;
-
-    cout << "\nAperte ENTER para continuar!\n";
-    getchar();
-    getchar();
+    Matrix matrix(0, 0);
+    matrix.readFromFile("DADOS.TXT");
+    cout << "\nMatriz:\n";
+    matrix.printar();
+    int procurado = 0;
+    cout << "Procurado:";
+    cin >> procurado;
+    int resultado = matrix.searchRow(procurado);
+    if(resultado != -1){
+    cout << "\nO numero se encontra na linha:" << resultado;
+    }else{
+    cout << "\nO numero nao existe";
+    }
+    close();
 }// Fim da função ex1018
 
 // Função para o exercício 0919
@@ -248,10 +445,20 @@ void ex1218() {
 void ex1219() {
     // identificacao
     cout << "\nExercicio 1219:\n\n";
-
-    cout << "\nAperte ENTER para continuar!\n";
-    getchar();
-    getchar();
+    Matrix matrix(0, 0);
+    matrix.readFromFile("DADOS.TXT");
+    cout << "\nMatriz:\n";
+    matrix.printar();
+    int procurado = 0;
+    cout << "Procurado:";
+    cin >> procurado;
+    int resultado = matrix.searchCol(procurado);
+    if(resultado != -1){
+    cout << "\nO numero se encontra na coluna:" << resultado;
+    }else{
+    cout << "\nO numero nao existe";
+    }
+    close();
 }// Fim da função ex0919
 
 // Função para o exercício 1020
@@ -267,10 +474,14 @@ void ex1219() {
 void ex1220() {
 // identificacao
     cout << "\nExercicio 1120:\n\n";
-
-    cout << "\nAperte ENTER para continuar!\n";
-    getchar();
-    getchar();
+    Matrix matrix(0, 0);
+    matrix.readFromFile("DADOS.TXT");
+    cout << "\nMatriz:\n";
+    matrix.printar();
+    Matrix transposta = matrix.transpose();
+    cout << "\nMatriz transposta:\n";
+    transposta.printar();
+    close();
 }// Fim da função ex0320
 
 // Função para o exercício 10E1
@@ -281,7 +492,16 @@ void ex1220() {
 
 void ex12E1(){
     cout << "\nExercicio 12E1:\n\n";
-
+    Matrix matrix(0, 0);
+    matrix.readFromFile("DADOS.TXT");
+    cout << "\nMatriz:\n";
+    matrix.printar();
+    bool teste = matrix.isCrescent();
+    if(teste){
+        cout << "A matriz eh crescente";
+    }else{
+        cout << "A matriz nao eh crescente";
+    }
 }
 // fim exercicio09E1
 // Função para o exercício 10E2
